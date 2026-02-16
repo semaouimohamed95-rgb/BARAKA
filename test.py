8647394220102b7f0d5b32db1caa19420c9974ef
semaouimohamed95-rgb <semaouimohamed95@gmail.com>
2026-02-16 14:09:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ FONT_PATH_BOLD = "NotoKufiArabic-Bold.ttf"
 # Positions
 POSITIONS = {
     "h1": (651, 470),
-    "name": (650, 545),
+    "name": (650, 600),
     "role": (652, 615),
     "body": 665
 }
@@ -213,3 +213,4 @@ if __name__ == "__main__":
         url_path=TELEGRAM_TOKEN,
         webhook_url=WEBHOOK_URL,
     )
+
